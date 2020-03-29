1ff57afc3ff4ea875893510ac6e362ab2350e03e
GuilhermeNovaes254 <guilherme.novaes@gmail.com>
2020-03-28 21:50:07 -0300

    adicacao das rotas para o Frontend

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,9 +1,85 @@
 const homeController = {
-    
+
   index: (req, res) => {
-    res.render('index',{ title: 'Home' });
-  }
-  
+    res.render('index', {
+      title: 'Home'
+    });
+  },
+
+  login: (req, res) => {
+    res.render('login', {
+      title: 'Login'
+    });
+  },
+
+  home2: (req, res) => {
+    res.render('home2', {
+      title: 'home2'
+    });
+  },
+
+  cadastroJogo: (req, res) => {
+    res.render('cadastro', {
+      title: 'cadastro'
+    });
+  },
+
+  perfil: (req, res) => {
+    res.render('perfil', {
+      title: 'perfil'
+    });
+  },
+
+  jogo: (req, res) => {
+    res.render('jogo', {
+      title: 'jogo'
+    });
+  },
+
+  busca: (req, res) => {
+    res.render('busca', {
+      title: 'busca'
+    });
+  },
+
+  cadastro: (req, res) => {
+    res.render('cadastro', {
+      title: 'cadastro'
+    });
+  },
+
+  perfilAdm: (req, res) => {
+    res.render('perfilAdm', {
+      title: 'perfilAdm'
+    });
+  },
+
+  perfilModerador: (req, res) => {
+    res.render('perfilModerador', {      title: 'perfilModerador'
+    });
+  },
+
+  moduloDestaques: (req, res) => {
+    res.render('moduloDestaques', {
+      title: 'moduloDestaques'
+    });
+  },
+
+  excluir: (req, res) => {
+    res.render('excluir', {
+      title: 'excluir'
+    });
+  },
+
+  editar: (req, res) => {
+    res.render('editar', {
+      title: 'editar'
+    });
+  },
+
+
 };
+
+
 
 module.exports = homeController;
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,18 @@ let homeController = require('../controllers/homeController');
 
 /* GET home page. */
 router.get('/', homeController.index);
+router.get('/login', homeController.login);
+router.get('/home2', homeController.home2);
+router.get('/cadastroJogo', homeController.cadastroJogo);
+router.get('/perfil', homeController.perfil);
+router.get('/jogo', homeController.jogo);
+router.get('/busca', homeController.busca);
+router.get('/cadastro', homeController.cadastro);
+router.get('/perfilAdm', homeController.perfilAdm);
+router.get('/perfilModerador', homeController.perfilModerador);
+router.get('/moduloDestaques', homeController.moduloDestaques);
+router.get('/excluir', homeController.excluir);
+router.get('/editar', homeController.editar);
+
 
 module.exports = router;
