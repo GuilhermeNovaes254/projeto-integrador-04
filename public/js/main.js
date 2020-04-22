0553494a67855a1375e32dedf2ac8266a1fe8d69
André Murakami <drehkenzo20@gmail.com>
2020-04-22 03:03:39 -0300

    modal landing - home

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,24 @@
 AOS.init();
+
+function initModal(){
+  var btEntrar = document.querySelector('.js-modal-login');
+
+  if(btEntrar){
+    btEntrar.addEventListener('click', openModal);
+  }
+}
+
+function openModal(){
+  var modalElement = document.querySelector('.o-modal-container');
+  var btClose = document.querySelector('.js-modal-close');
+
+  if(modalElement){
+    modalElement.classList.add('js-active-modal');
+
+    btClose.addEventListener('click', function(){
+      modalElement.classList.remove('js-active-modal');
+    });
+  }
+}
+
+window.onload = initModal;
