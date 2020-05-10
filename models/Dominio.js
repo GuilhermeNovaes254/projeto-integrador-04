6475c7c7a3efbf33590a5f89e1ade35452fbb551
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-05-10 16:53:55 -0300

    Model Dominio

---
+++ models/Dominio.js
@@ -0,0 +1,28 @@
+const Dominio = (sequelize, DataTypes) => {
+    var dominio = sequelize.define(
+        'Dominio',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+                autoIncrement: true,
+                allowNull:false,
+                unique: true
+            },
+            nome: {
+                type: DataTypes.STRING(80),
+                allowNull: true
+            }
+        },{
+            tableName: "dominio",
+            timestamps: false
+        }
+
+    );
+    dominio.associate = (models) => {
+        dominio.hasMany(models.Jogo, {foreignKey: 'dominio_id', as:'jogos'})
+    }
+    return dominio;
+}
+
+module.exports = Dominio;
