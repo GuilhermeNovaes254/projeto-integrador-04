4e85e6fc08737e9ce9abf57b99ad286b090113ed
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-05-13 00:21:30 -0300

    migration table estado

--- migrations/20200513001857-create_table_estado.js
+++ migrations/20200513001857-create_table_estado.js
@@ -2,22 +2,24 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable(
+      'estado',
+      { 
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull:false,
+          unique: true
+      },
+      sigla: {
+          type: Sequelize.STRING(2),
+          allowNull: false
+      }
+      });
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('estado');
   }
 };
