b1891d8bd92d652287186d6e65fdfa8bfd7699d3
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-05-13 00:17:45 -0300

    migration table mecanica

--- migrations/20200513001907-create_table_mecanica.js
+++ migrations/20200513001907-create_table_mecanica.js
@@ -2,22 +2,24 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable(
+      'mecanica',
+      { 
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull:false,
+          unique: true
+      },
+      nome: {
+          type: Sequelize.STRING(80),
+          allowNull: true
+      }
+      });
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('mecanica');
   }
 };
