d80e4b036d1de493d8392f63a1d4dd07330080e3
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-05-13 00:31:33 -0300

    migration table cidade

--- migrations/20200513001859-create_table_cidade.js
+++ migrations/20200513001859-create_table_cidade.js
@@ -2,22 +2,34 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+    return queryInterface.createTable(
+      'cidade',
+      { 
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+          allowNull:false,
+          unique: true
+      },
+      cidade: {
+          type: Sequelize.STRING(200),
+          allowNull: false
+      },
+      estado_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'estado',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }
+      });
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return queryInterface.dropTable('cidade');
   }
 };
