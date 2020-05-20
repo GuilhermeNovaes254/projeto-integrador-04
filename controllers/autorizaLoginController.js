bf224c5b89bc7bc2799434632b959cb320065c51
GuilhermeNovaes254 <guilherme.novaes@gmail.com>
2020-05-19 23:13:54 -0300

    updates da Busca, dificuldd pra capturar o seletor

--- controllers/autorizaLoginController.js
+++ controllers/autorizaLoginController.js
@@ -30,7 +30,7 @@ const autorizaLogin = {
         
 
         // redirecionar o suaurio para a rota '/feeds'*/
-        res.redirect('/feeds')   
+        res.redirect('/busca')   
     }
 
 
--- public/js/scriptBusca.js
+++ public/js/scriptBusca.js
@@ -1,41 +1,28 @@
 function tipo() {
 
-    let select = document.getElementById('tipo');
-    console.log(select.innerText)
-    var xhr = new XMLHttpRequest();
+    let select = document.getElementsByName
 
-    if (select == 'jogo') {
-        console.log('jogo')
-        /*  
-        // Abre os próximo pop-ups
-        // Process our return data
-        if (xhr.status >= 200 && xhr.status < 300) {
-            let select = document.getElementById('tipo');
-            select.innerText = '';
-            select.innerHTML = '<option value="">Selecione</option>';
+        console.log(select)
 
+        if (select == 'jogo') {
 
+            console.log('jogo')
 
-            for (let estado of estados) {
-                select.innerHTML += `<option value="${estado.id}">${estado.sigla}</option>`
-            }
+            let elementoJogo = document.getElementById('buscaJogo').style.visibility = "visible";
+            let elementoUsuario = document.getElementById('buscaUsuario').style.visibility = "hidden";
 
         }
-        // Create and send a GET request
-        // The first argument is the post type (GET, POST, PUT, DELETE, etc.)
-        // The second argument is the endpoint URL
+        
+        if (select == 'usuario') {
+            // Abre barra de pesquisa para nome
+            // console.log('done')
+            let elementoJogo = document.getElementById('buscaJogo').style.visibility = "hidden";haha
+            let elementoUsuario = document.getElementById('buscaUsuario').style.visibility = "visible";
 
-        xhr.open('GET', '/buscaEstado');
-        xhr.send();
-*/
-    } else {
-        // Abre barra de pesquisa para nome
-        console.log('done')
-        // select.innerHTML += "<input type='text'  class='form-control inputCadastro' id='busca' placeholder='Buscar'  name='nome'>"
+            // consulta o banco que traz sobre as pessoas
 
-        // consulta o banco que traz sobre as pessoas
-
-    }
+        }
+    
 }
 
 tipo()
