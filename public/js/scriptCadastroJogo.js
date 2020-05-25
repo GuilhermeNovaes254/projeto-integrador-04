6fda1d4f35f6bec15448fbf7a7c5be4a4dddd8c8
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-05-24 21:29:22 -0300

    cadJogo mudar foto tema na tela ao selecionar nova

--- public/js/scriptCadastroJogo.js
+++ public/js/scriptCadastroJogo.js
@@ -25,6 +25,8 @@ for (let i = min2; i <= max2; i++) {
 
 let fotoPerfil = document.getElementById('file-input');
 let imgFotoPerfil = document.getElementById('imgFotoPerfil');
+let fotoTema = document.getElementById('inputFileTemaJogo');
+let imgFotoTema = document.getElementById('temaCadastroJogo');
 
 fotoPerfil.addEventListener('change', function (evt) {
 
@@ -36,6 +38,22 @@ fotoPerfil.addEventListener('change', function (evt) {
         var fr = new FileReader();
         fr.onload = function () {
             imgFotoPerfil.src = fr.result;
+        }
+        fr.readAsDataURL(files[0]);
+    }
+})
+
+
+fotoTema.addEventListener('change', function (evt) {
+
+    var tgt = evt.target || window.event.srcElement,
+        files = tgt.files;
+
+    // FileReader support
+    if (FileReader && files && files.length) {
+        var fr = new FileReader();
+        fr.onload = function () {
+            imgFotoTema.style.backgroundImage = "url('"+fr.result+"')"
         }
         fr.readAsDataURL(files[0]);
     }
