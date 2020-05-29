ed00c47f131f9e0040592ba2cf712879d385d1c6
GuilhermeNovaes254 <guilherme.novaes@gmail.com>
2020-05-28 21:32:44 -0300

    Busca recebe json com jogos e front recebe os dados

--- public/js/scriptBusca.js
+++ public/js/scriptBusca.js
@@ -21,11 +21,11 @@ function consultaUsuario(usuario) {
 
             }
 
-            for (let i = 1; i <= resposta.length; i++) {
+            for (let i = 0; i < resposta.length; i++) {
 
-                let tabela1 = "<tr><img id='foto' src=" + respostas.resposta[i].foto + "></tr>";
-                let tabela2 = "<li>Nome: " + respostas.resposta[i].nome + "</li>";
-                let tabela3 = "<li>Apelido: " + respostas.resposta[i].apelido + "</li>";
+                let tabela1 = "<tr><img id='foto' src=" + resposta[i].foto + "></tr>";
+                let tabela2 = "<li>Nome: " + resposta[i].nome + "</li>";
+                let tabela3 = "<li>Apelido: " + resposta[i].apelido + "</li>";
                 table.innerHTML = table.innerHTML + cabecalho + tabela1 + tabela2 + tabela3
 
             }
@@ -59,86 +59,87 @@ function consultaJogo(tema, dominio, mecanica) {
             table.innerText = '';
             table.innerHTML = "<table id='table'>";
 
-            table.innerHTML = "<table id='table'>"/*
-            if (respostas.resTema.length == [] && respostas.resDominio.length == [] && respostas.resMecanica.length == []) {
+            table.innerHTML = "<table id='table'>"
+            if (respostas.length == []) {
                 table.innerHTML = table.innerHTML + "<h2>Não foram encontrados dados</h2>"
                 table.innerHTML = table.innerHTML + "</table>"
-            }
-            else {
-
-                if(respostas.resTema != []) {
-                    for (let i = 0; i < respostas.length; i++) {
-            /*
-                        if (respostas.resposta[i].foto != null) {
-                            let tabela1 = "<tr><img id='foto' src=" + respostas.resposta[i].foto + "></tr>";
-                            table.innerHTML = table.innerHTML + tabela1
-                        }
-
-                        let tabela3 = "<li>Nome: " + respostas.resposta[i].nome + "</li>";
-                        table.innerHTML = table.innerHTML + tabela3
-
-                        if (respostas.resposta[i].ano != null) {
-                            let tabela4 = "<li>Ano: " + respostas.resposta[i].ano + "</li>";
-                            table.innerHTML = table.innerHTML + tabela4
-                        }
-
-                        if (respostas.resposta[i].faixaEtaria != null) {
-                            let tabela5 = "<li>Faixa Etária: " + respostas.resposta[i].faixaEtaria + "</li>";
-                            table.innerHTML = table.innerHTML + tabela5
-                        }
-
-                        if (respostas.resposta[i].duracao != null) {
-                            let tabela6 = "<li>Duração: " + respostas.resposta[i].duracao + "</li>";
-                            table.innerHTML = table.innerHTML + tabela6
-                        }
-
-                        if (respostas.resposta[i].downtime != null) {
-                            let tabela7 = "<li>Downtime: " + respostas.resposta[i].downtime + "</li>";
-                            table.innerHTML = table.innerHTML + tabela7
-                        }
-
-                        if (respostas.resposta[i].notaJogo != null) {
-                            let tabela8 = "<li>Nota Jogo: " + respostas.resposta[i].notaJogo + "</li>";
-                            table.innerHTML = table.innerHTML + tabela8
-                        }
-
-                        if (respostas.resposta[i].qntMax != null) {
-                            let tabela9 = "<li>Máximo Jogadores: " + respostas.resposta[i].qntMax + "</li>";
-                            table.innerHTML = table.innerHTML + tabela9
-                        }
-
-                        if (respostas.resposta[i].qntMin != null) {
-                            let tabela10 = "<li>Mínimo Jogadores: " + respostas.resposta[i].qntMin + "</li>";
-                            table.innerHTML = table.innerHTML + tabela10
-                        }
+            } else {
 
 
-                        let urlFetch = '/tema?id='+ respostas.resTema[i].tema_id
-                        fetch(urlFetch, { 
-                            method: 'get' // opcional 
-                          })
-                          .then(function(response) { 
-                            // use a resposta 
-                          })
-                          .catch(function(err) { console.error(err); });
+                for (let i = 0; i < respostas.length; i++) {
 
-                        let tabela11 = "<li>Tema: " + respostas.resposta[i].tema_id + "</li>";
-                        table.innerHTML = table.innerHTML + tabela11
-
-                        let tabela12 = "<li>Domínio: " + respostas.resposta[i].dominio_id + "</li>";
-                        let tabela13 = "<li>Mecânica: " + respostas.resposta[i].mecanica_id + "</li>";
-                        table.innerHTML = table.innerHTML + tabela11 + tabela12 + tabela13
-
-
-                        if (respostas.resposta[i].descricao != null) {
-                            let tabela14 = "<li>Descrição: " + respostas.resposta[i].descricao + "</li>";
-                            table.innerHTML = table.innerHTML + tabela14
-                        }
-
+                    if (respostas[i].foto != null) {
+                        let tabela1 = "<tr><img id='foto' src=" + respostas[i].foto + "></tr>";
+                        table.innerHTML = table.innerHTML + tabela1
                     }
+
+                    let tabela3 = "<li>Nome: " + respostas[i].nome + "</li>";
+                    table.innerHTML = table.innerHTML + tabela3
+
+                    if (respostas[i].ano != null) {
+                        let tabela4 = "<li>Ano: " + respostas[i].ano + "</li>";
+                        table.innerHTML = table.innerHTML + tabela4
+                    }
+
+                    if (respostas[i].faixaEtaria != null) {
+                        let tabela5 = "<li>Faixa Etária: " + respostas[i].faixaEtaria + "</li>";
+                        table.innerHTML = table.innerHTML + tabela5
+                    }
+
+                    if (respostas[i].duracao != null) {
+                        let tabela6 = "<li>Duração: " + respostas[i].duracao + "</li>";
+                        table.innerHTML = table.innerHTML + tabela6
+                    }
+
+                    if (respostas[i].downtime != null) {
+                        let tabela7 = "<li>Downtime: " + respostas[i].downtime + "</li>";
+                        table.innerHTML = table.innerHTML + tabela7
+                    }
+
+                    if (respostas[i].notaJogo != null) {
+                        let tabela8 = "<li>Nota Jogo: " + respostas[i].notaJogo + "</li>";
+                        table.innerHTML = table.innerHTML + tabela8
+                    }
+
+                    if (respostas[i].qntMax != null) {
+                        let tabela9 = "<li>Máximo Jogadores: " + respostas[i].qntMax + "</li>";
+                        table.innerHTML = table.innerHTML + tabela9
+                    }
+
+                    if (respostas[i].qntMin != null) {
+                        let tabela10 = "<li>Mínimo Jogadores: " + respostas[i].qntMin + "</li>";
+                        table.innerHTML = table.innerHTML + tabela10
+                    }
+
+
+                   /* let urlFetch = '/tema?id=' + respostas[i].tema_id
+                    fetch(urlFetch, {
+                            method: 'get' // opcional 
+                        })
+                        .then(function (response) {
+                            // use a resposta 
+                        })
+                        .catch(function (err) {
+                            console.error(err);
+                        });
+*/
+                    let tabela11 = "<li>Tema: " + respostas[i].tema_id + "</li>";
+                    table.innerHTML = table.innerHTML + tabela11
+
+                    let tabela12 = "<li>Domínio: " + respostas[i].dominio_id + "</li>";
+                    let tabela13 = "<li>Mecânica: " + respostas[i].mecanica_id + "</li>";
+                    table.innerHTML = table.innerHTML + tabela11 + tabela12 + tabela13
+
+
+                    if (respostas[i].descricao != null) {
+                        let tabela14 = "<li>Descrição: " + respostas[i].descricao + "</li>";
+                        table.innerHTML = table.innerHTML + tabela14
+                    }
+
+
                 }
                 table.innerHTML = table.innerHTML + "</table>"
-            }*/
+            }
         } else {
             console.log('erro na Api')
         }
