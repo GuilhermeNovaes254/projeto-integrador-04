9470e517eff61e263bb04d7b75e735b3adcfc581
GuilhermeNovaes254 <guilherme.novaes@gmail.com>
2020-05-30 22:27:19 -0300

    busca por nome refatorada

--- controllers/buscaController.js
+++ controllers/buscaController.js
@@ -10,33 +10,42 @@ const {
 const buscaController = {
 
         usuarios: async (req, res) => {
+                try {
+                        let {
+                                item
+                        } = req.params;
 
-                let {
-                        item
-                } = req.params;
 
-                let user = await Usuario.findAll({
-                        where: {
-                                nome: {
+                        let busca = []
+
+                        let whereClause = {};
+
+                        if (item != '') {
+                                whereClause['nome'] = {
                                         [Op.like]: '%' + item + '%'
-                                }
-                        },
-                        order: [
-                                ['nome', 'ASC']
-                        ]
-                }).then(users => {
-                        return res.status(200).json(users)
-                }).catch(error => {
-                        return res.status(400).json(error)
-                })
+                                };
+                        }
 
 
-                res.send(user);
+                        busca = await Usuario.findAll({
+                                order: [
+                                        ['nome', 'ASC']
+                                ],
+                                where: whereClause
+                        })
+
+
+                        res.send(busca)
+
+                } catch (error) {
+                        res.status(401)
+                }
         },
+
 
         jogos: async (req, res) => {
                 try {
-                        let {   
+                        let {
                                 nome,
                                 tema,
                                 dominio,
@@ -46,35 +55,35 @@ const buscaController = {
                         let busca = []
 
 
-                        let whereClause ={};
+                        let whereClause = {};
 
                         if (nome != '') {
                                 console.log("daniele");
-                                whereClause['nome'] = { 
-                                  [Op.like]: '%' + nome + '%'
+                                whereClause['nome'] = {
+                                        [Op.like]: '%' + nome + '%'
                                 };
                         }
 
-                        if(tema != 0){
+                        if (tema != 0) {
                                 whereClause['tema_id'] = tema;
                         }
 
-                        if(dominio != 0){
+                        if (dominio != 0) {
                                 whereClause['dominio_id'] = dominio;
                         }
 
-                        if(mecanica != 0){
+                        if (mecanica != 0) {
                                 whereClause['mecanica_id'] = mecanica;
                         }
 
                         console.log(whereClause);
                         // if (tema != 0 && dominio != 0 && mecanica != 0) {
-                                busca = await Jogo.findAll({
-                                        order: [
-                                                ['nome', 'ASC']
-                                              ],
-                                        where: whereClause
-                                })
+                        busca = await Jogo.findAll({
+                                order: [
+                                        ['nome', 'ASC']
+                                ],
+                                where: whereClause
+                        })
                         // }
 
                         res.send(busca)
