f92d1ce57176a023d3c6fe08634c1f828c4037d3
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-06-13 21:03:27 -0300

    fix model usuario

--- models/Usuario.js
+++ models/Usuario.js
@@ -24,7 +24,7 @@ const Usuario = (sequelize, DataTypes) => {
             },
             foto:{
                 type: DataTypes.STRING(500),
-                allowNull: true
+                allowNull: true,
                 default:'./images/default/jogoDefault.png'
             },
             descricao:{
