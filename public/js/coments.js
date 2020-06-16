79f4add77b7367bddef7b275014212f51844fda9
André Murakami <drehkenzo20@gmail.com>
2020-06-16 01:48:40 -0300

    implementacoes em comentarios / ajuste em foto de perfil e menu

--- public/js/coments.js
+++ public/js/coments.js
@@ -5,4 +5,14 @@ page = page[page.length-1];
 if(page.split('?')[0] == 'jogo'){
   let writeComment = document.querySelector('.c-write');
   writeComment.classList.add('js-activeWrite');
+
+  let spaceComment = document.querySelector('.c-coments__box--header');
+  spaceComment.classList.add('js-spaceComment');
+
+  let featuresComment = document.querySelectorAll('.c-coments__features');
+
+  featuresComment.forEach(el => {
+    el.classList.add('js-hiddenEl');
+  });
+  
 }
